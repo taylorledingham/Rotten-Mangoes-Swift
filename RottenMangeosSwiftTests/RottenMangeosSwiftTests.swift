c24743837ec2223c57108769653cc2f2b5224eb9
taylor.ledingham <taylor.ledingham@gmail.com>
2014-12-01 18:08:25 -0800

    Initial Commit

---
+++ RottenMangeosSwiftTests/RottenMangeosSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  RottenMangeosSwiftTests.swift
+//  RottenMangeosSwiftTests
+//
+//  Created by Taylor Ledingham on 2014-12-01.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RottenMangeosSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
